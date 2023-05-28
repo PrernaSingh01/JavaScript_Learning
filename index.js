b5df6d689d3e513eafa11a2ee4bfd287e96eae68
Prerna <iprernasingh4@gmail.com>
2023-05-28 20:32:26 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+//console.log("Hello world");//
+
+/**👉 Values and Variables **/
+
+//var myName = "Prerna Singh";
+//console.log(myName);
+
+/** Naming Practiice **/
+//var _myName = "Prerna";
+//var 1myName = "Prerna";
+//var _1my__Name = "Prerna";
+//var $myName = "Prerna";
+
+//console.log($myName);
+
+/**DataTypes **/

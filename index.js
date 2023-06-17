75edb11b1589c65ccaa36d41dce793046dae4ca0
Prerna <iprernasingh4@gmail.com>
2023-06-17 20:58:16 +0530

    first commit

--- index.js
+++ index.js
@@ -830,3 +830,28 @@ bioData.getData();*/
 //Scope chain
 
 //Lexical Scoping
+
+function func1() {
+  setTimeout(() => {
+    console.log(x);
+    console.log(y);
+  }, 3000);
+
+  var x = 2;
+  let y = 12;
+}
+func1();
+
+/*function func2() {
+  for (var i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 2000);
+  }
+}
+func2();*/
+
+/*(function () {
+  setTimeout(() => console.log(1), 2000);
+  console.log(2);
+  setTimeout(() => console.log(3), 0);
+  console.log(4);
+})();*/
